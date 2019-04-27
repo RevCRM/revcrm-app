dfa12e4495d7be657efd7d922c244e2298876884
Russell Briggs <russ@paraflyer.net>
2019-04-27 14:45:35 +1200

    move data mounts to /mnt/odoo to make NFS easier

--- tag+push.sh
+++ tag+push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.0.0
+VERSION=1.0.1
 echo "Pushing RevCRM v$VERSION"
 docker tag revcrm:latest gcr.io/macro-duality-233703/revcrm:$VERSION
 docker push gcr.io/macro-duality-233703/revcrm:$VERSION
