7a8280478652bd2d5de17e392fcfae20262ab8b4
Russell Briggs <russ@paraflyer.net>
2019-04-27 14:02:54 +1200

    Store version number with tag script

--- tag+push.sh
+++ tag+push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker tag revcrm:latest gcr.io/macro-duality-233703/revcrm:$1
-docker push gcr.io/macro-duality-233703/revcrm:$1
+VERSION=1.0.0
+echo "Pushing RevCRM v$VERSION"
+docker tag revcrm:latest gcr.io/macro-duality-233703/revcrm:$VERSION
+docker push gcr.io/macro-duality-233703/revcrm:$VERSION
